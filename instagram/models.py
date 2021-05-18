79f3ce3c75a7f31daf089f02ea667f07ac14010a
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-18 15:45:31 +0300

    created a profile model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_photo = models.ImageField(upload_to='images/', default='DEFAULT VALUE')
+    bio = models.TextField(max_length=300, default="My Bio", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
