291ad803c58124742872295e3cea61fe7ed90366
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-19 07:32:12 +0300

    Created update profile form class

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from .models import Profile, Image, Comment
+from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ('image', 'caption')
+
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'profile_photo', 'bio']
