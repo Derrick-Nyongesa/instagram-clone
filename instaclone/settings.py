61d551b58c74a7ba974fb24d9502993e615170f3
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-22 17:39:51 +0300

    configured Django secret key

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bn=l2%q6198ua=(kc4josprdl#lb5a1smgwt0wdj^a4w!^-ilj'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
